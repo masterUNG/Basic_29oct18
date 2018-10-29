ce415755a80bbe01e2de661a555694f37e392501
masterUNG <phrombutr@gmail.com>
2018-10-29 09:33:47 +0700

    var and constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//นี่คือการประกาศตัวแปรแบบ Implicit
+
+var nameString = "MasterUNG"    //นี่คือการประกาศ nameString ที่มีค่า MasterUNG เป็น String Type
+
+var number1Int = 100
+var number2Dou = 12.34
+var status1Bool = true
+
+//Explicit Type
+var name2String: String = "Doramon" // Explicit คือการประกาศ แบบกำหนด Data Type
+var number3Int: Int = 123
+var status2Bool: Bool = false
+var number4Dou:Double = 123.45
+
+print("Current NameString ==> \(nameString)")
+nameString = "Nopita"
+print("Current NameString ==> \(nameString)")
+
+//การประกาศค่าคงที่
+let surnameString: String = "Phrombutr"
+//surnameString = "AAAA" ไม่สามารถ Assign Value 
+
+
+
+
+
